5ac8f24c3700d7f01c1d93a36295478ca9980292
Ararat1 <matinyan_0@mail.ru>
2021-04-03 12:43:41 +0400

    Some changes in <Logo />

--- src/Components/App/Header/Logo/Logo.jsx
+++ src/Components/App/Header/Logo/Logo.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 import s from "./Logo.module.sass";
 
 const Logo = () => {
     return (
         <div className={`${s.Logo} col-12 col-sm-6 col-md-5 col-lg-4`}>
-            <h2>Contact Manager</h2>
+            <h2>
+                <Link to="/">Contact Manager</Link>
+            </h2>
         </div>
     );
 };
