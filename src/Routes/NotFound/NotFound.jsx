119bb31963d03d4fa06990b933abe87c57bdfbaf
Ararat1 <matinyan_0@mail.ru>
2021-04-04 17:20:16 +0400

    Updated <NotFound /> component

--- src/Routes/NotFound/NotFound.jsx
+++ src/Routes/NotFound/NotFound.jsx
@@ -1,10 +1,18 @@
-const NoteFound = () => {
+import { Link } from "react-router-dom";
+
+import s from "./NotFound.module.sass";
+
+const NotFound = () => {
     return (
-        <main>
+        <main className={s.NotFound}>
             <div className="container">
                 <div className="row">
                     <div className="col-12 text-center">
-                        <h1>Page is not found ..</h1>
+                        <span>
+                            <i className="fas fa-ban"></i>
+                        </span>
+                        <h1>This page is not available</h1>
+                        <Link to="/">Go to the homepage</Link>
                     </div>
                 </div>
             </div>
@@ -12,4 +20,4 @@ const NoteFound = () => {
     );
 };
 
-export default NoteFound;
+export default NotFound;
