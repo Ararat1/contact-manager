bd5a1c6760cacb4ebd22a4d3fb5e7c9234097f60
Ararat1 <matinyan_0@mail.ru>
2021-04-04 18:03:54 +0400

    Added Sticky Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import s from "./Footer.module.sass";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+
     return (
         <footer className={s.Footer}>
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <div className="p">&copy; Contact Manager</div>
+                        <div className="p">
+                            Copyright &copy; {year} | Contact Manager
+                        </div>
                     </div>
                 </div>
             </div>
